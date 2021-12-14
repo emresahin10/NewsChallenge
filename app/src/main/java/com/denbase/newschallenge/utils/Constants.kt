00501a9d3dd800e1203c9172d566648475814fee
Emre Şahin <47294155+emresahin10@users.noreply.github.com>
2021-12-14 15:35:44 +0300

    Update Constants.kt

--- app/src/main/java/com/denbase/newschallenge/utils/Constants.kt
+++ app/src/main/java/com/denbase/newschallenge/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.denbase.newschallenge.utils
 
 class Constants {
     companion object{
-        val API_KEY = "c092edc3945c4d2d9cc8ee1a12a8a734"
+        val API_KEY = "YOUR KEY"
         val BASE_URL = "https://newsapi.org/v2/"
     }
 }
